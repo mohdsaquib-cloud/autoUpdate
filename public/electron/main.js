1313fe17a7dc4e8fa57678e1fda7cb32e6ac0721
Saquib <mohdsaquib9876@gmail.com>
2021-02-25 18:27:39 +0530

    autoupdated

--- public/electron/main.js
+++ public/electron/main.js
@@ -1,28 +1,53 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+let mainWindow;
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            contextIsolation: false,
+            nodeIntegration: true,
         },
     });
     const StartUrl =
         process.env.url ||
         `file://${path.join(__dirname, "../../build/index.html")}`;
     mainWindow.loadURL(StartUrl);
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+    mainWindow.once("ready-to-show", () => {
+        autoUpdater.checkForUpdatesAndNotify();
+    });
 }
 
-app.whenReady().then(() => {
+autoUpdater.on("update-available", () => {
+    mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+    mainWindow.webContents.send("update_downloaded");
+});
+ipcMain.on("restart_app", () => {
+    autoUpdater.quitAndInstall();
+});
+app.on("ready", () => {
     createWindow();
-
-    app.on("activate", function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
 });
 
 app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit();
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+ipcMain.on("app_version", (event) => {
+    event.sender.send("app_version", { version: app.getVersion() });
 });
