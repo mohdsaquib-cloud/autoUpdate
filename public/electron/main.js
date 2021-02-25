f37a3a151768cf83ceee8861bef34da9794ddac2
Saquib <mohdsaquib9876@gmail.com>
2021-02-25 18:37:42 +0530

    change

--- public/electron/main.js
+++ public/electron/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const { autoUpdater } = require("electron-updater");
+const path = require("path");
 
 let mainWindow;
 
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App(props) {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Hello world</h1>
         </div>
     );
 }
